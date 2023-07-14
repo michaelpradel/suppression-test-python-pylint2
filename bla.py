94ab65c87960d4cd21d416620ebc29a5374a57a2
Michael Pradel <michael@binaervarianz.de>
2023-07-14 10:54:25 +0200

    Inserting code that moves the existing warning further down in the file

--- bla.py
+++ bla.py
@@ -1,5 +1,9 @@
 """This is a module bla.py"""
 
+def filler():
+    """This is a filler function"""
+    return True
+
 def hello():
     """Prints a hello message"""
     print("Hello from bla.py" + foo)
